609869d8751d55b3c7311f776d31e3e0e3ce2067
James <james689@users.noreply.github.com>
2019-11-17 18:18:26 +0000

    Change to generic monospaced font family name as courier new may not be available on all platforms.

--- src/MatrixRainGUI.java
+++ src/MatrixRainGUI.java
@@ -16,10 +16,8 @@ import javax.swing.Timer;
  * The MatrixRainGUI class is the JPanel used to display the matrix rain effect.
  * This panel is divided into a grid of rows and columns, the specific number
  * of rows and columns used depends upon the font size of the text to be displayed
- * and the dimensions of the panel. So, for example, using a smaller font size
- * or resizing the window the make the panel larger will result in more rows/columns.
- * Each cell in the grid is big enough to display a single character. Each cell's width/height
- * is the same size as the font width/height. The program assumes a monospaced font is
+ * and the dimensions of the panel. Each cell in the grid is the same width and height as
+ * the font width/height and so can display a single character. The program assumes a monospaced font is
  * being used i.e. a font where all characters have the same width.
  */
 public class MatrixRainGUI extends JPanel {
@@ -31,19 +29,14 @@ public class MatrixRainGUI extends JPanel {
     private int nMaxStreamers = 200; // maximum number of streamers that can appear on screen
     private int numCols, numRows; // the MatrixRainGUI panel is divided into a grid of size
     // numRows*numCols. Each cell in the grid has space to display a single character.
-    private Font monospaceFont = new Font("Courier New", Font.PLAIN, 15); // font used to display
+    private Font monospaceFont = new Font("Monospaced", Font.PLAIN, 15); // font used to display
     // the characters on screen
     private Dimension oldPanelSize = new Dimension(400, 400); // used to detect whether
     // there has been a change in the panel size.
-    private boolean[][] charMatrix;
-    //private char[][] charMatrix; // this is used so that only one character can
-    // be drawn in a cell on the screen, to prevent the Streamer's from
-    // overlapping each other. Each Streamer's characters are added to the charMatrix,
-    // potentially overwriting characters from other streamers already existing in the
-    // char matrix. The charMatrix can then be blitted to the screen.
-    private boolean useCharMatrixRendering = false; // gives the option to switch between
-    // either rendering streamers with the potential to overlap/be drawn on top of each other (i.e. non
-    // matrix rendering, or drawing non-overlapping streamers (matrix rendering)
+    private boolean[][] cellsDrawnIn; // used to determine whether a cell in the grid already
+    // contains a character, and thus prevent drawing multiple characters to the same position
+    // on the screen, which can result in a not pleasant looking overlapping effect.
+    private int charWidth, charHeight; // width and height of a single character in the given font
 
     public MatrixRainGUI() {
         setPreferredSize(oldPanelSize);
@@ -61,12 +54,15 @@ public class MatrixRainGUI extends JPanel {
     private class Streamer {
 
         int column = 0; // x coordinate of streamer (in columns)
-        float fPosition = 0.0f; // y coordinate of the head of the streamer (in rows)
-        // (the head of the streamer is the character closest to the bottom of the screen)
-        double fSpeed = 10.0; // speed the streamer falls at
+        float row = 0.0f; // y coordinate of the head of the streamer (in rows)
+        // the streamer can be positioned in between rows e.g. at row 5.45, to create a smoother animation
+        // so that the streamer can move in smaller increments than 1 row at a time,
+        // but it will always be drawn on an integer boundary. (the head of the streamer is the 
+        // character closest to the bottom of the screen)
+        double speed = 10.0; // speed the streamer falls at (in pixels per second)
         StringBuilder text = new StringBuilder(); // text of the streamer, use
         // StringBuilder instead of String to make appending random characters to the streamer
-        // more efficient.
+        // in the prepareStreamer() method more efficient.
 
         public Streamer() {
             prepareStreamer();
@@ -75,11 +71,9 @@ public class MatrixRainGUI extends JPanel {
         // resets the Streamer to a new random position, speed and text
         public void prepareStreamer() {
             // set position and speed of streamer
-            column = HelperMethods.mathRandom(0,numCols); // start streamer at a random column
-            fPosition = 0.0f; // always start streamer at the top row of the screen
-            fSpeed = HelperMethods.mathRandom(5,46); // random speed between 5 and 45
-            //fSpeed = 2; // fixed speed for debugging
-            
+            column = HelperMethods.mathRandom(0,numCols); // start streamer at a random column/x coordinate
+            row = 0.0f; // always start streamer at the top row of the screen
+            speed = HelperMethods.mathRandom(5,46); // random speed between 5 and 45
             // set text of streamer
             text.setLength(0); // clear the string builder's previous text (see https://stackoverflow.com/questions/5192512/how-can-i-clear-or-empty-a-stringbuilder)
             //text.append("ABCDEFGHIJKLMNO"); // if you want the streamer to have fixed text for debugging
@@ -87,25 +81,23 @@ public class MatrixRainGUI extends JPanel {
             for (int i = 0; i < nStreamerLength; i++) {
                 text.append(randomCharacter());
             }
-            System.out.println("in prepareStreamer(). column = " + column + " fSpeed = " + fSpeed + " streamerLength = " + nStreamerLength);
         }
     }
     
     // called either when there are no existing streamers or when the panel is resized
     // and new streamers need to be created.
-    private void createStreamers(int charWidth, int charHeight) {
-        System.out.println("create streamers called");
+    private void createStreamers() {
         listStreamers = new ArrayList<>();
         double panelWidth = getWidth();
         double panelHeight = getHeight();
-        System.out.println("panel width = " + panelWidth + " panel height = " + panelHeight);
+        //System.out.println("panel width = " + panelWidth + " panel height = " + panelHeight);
         // calculate how many rows and columns the panel can support
-        // based upon the font size.
+        // based upon the font size. Use Math.ceil() so that an extra row/column is used
+        // to prevent having an unsightly gap at the edges of the screen.
         numCols = (int) Math.ceil(panelWidth / charWidth);
         numRows = (int) Math.ceil(panelHeight / charHeight);
-        //charMatrix = new char[numRows][numCols];
-        charMatrix = new boolean[numRows][numCols];
-        System.out.println("numCols = " + numCols + " numRows = " + numRows);
+        cellsDrawnIn = new boolean[numRows][numCols];
+        //System.out.println("numCols = " + numCols + " numRows = " + numRows);
         for (int i = 0; i < nMaxStreamers; i++) {
             listStreamers.add(new Streamer());
         }
@@ -128,10 +120,10 @@ public class MatrixRainGUI extends JPanel {
             lastTimeMillis = currentTimeMillis;
 
             for (Streamer s : listStreamers) {
-                s.fPosition += s.fSpeed * elapsedTimeSeconds; // update position of streamer based upon elapsed time
-                //System.out.println("numRows = " + numRows + " numCols = " + numCols + " fPosition: " + s.fPosition);
+                s.row += s.speed * elapsedTimeSeconds; // update position of streamer based upon elapsed time
+                //System.out.println("numRows = " + numRows + " numCols = " + numCols + " row: " + s.row);
                 
-                if (s.fPosition - s.text.length() > numRows) {
+                if (s.row - s.text.length() > numRows) {
                     // the tail of the streamer has gone past the bottom edge of the screen
                     // so reset the streamer
                     s.prepareStreamer();
@@ -140,59 +132,60 @@ public class MatrixRainGUI extends JPanel {
             repaint();
         }
     }
-
-    // draws the panel
+    
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setFont(monospaceFont);
-        FontMetrics metrics = g.getFontMetrics();
-        /* debugging code to check that all characters have the same width
-        for (char c = 'a'; c <= 'z'; c++) {
-            System.out.println("width of " + c + " = " + metrics.charWidth(c));
-        }*/
-        int charWidth = metrics.charWidth('c'); // all characters in a monospace
-        // font should have the same width, so just pick any character to base
-        // the charWidth off of
-        //System.out.println("charWidth = " + charWidth);
-        int charHeight = metrics.getAscent() + metrics.getDescent();
-        //System.out.println("charHeight = " + charHeight);
-        
         Dimension currentPanelSize = getSize();
         // re-populate the list of streamers if either this is the first time
         // creating the streamers or the panel has been resized
         if (listStreamers == null || !currentPanelSize.equals(oldPanelSize)) {
-            createStreamers(charWidth, charHeight);
+            FontMetrics metrics = g.getFontMetrics();
+            /* debugging code to check that all characters have the same width
+            for (char c = 'a'; c <= 'z'; c++) {
+                System.out.println("width of " + c + " = " + metrics.charWidth(c));
+            }*/
+            charWidth = metrics.charWidth('c'); // all characters in a monospace
+            // font should have the same width, so just pick any character to base
+            // the charWidth off of
+            charHeight = metrics.getHeight();
+            createStreamers();
             oldPanelSize = currentPanelSize;
         }
 
-        // draw streamers on screen
         Graphics2D g2d = (Graphics2D) g;
-        if (useCharMatrixRendering) {
-            /*clearCharMatrix();
-            for (Streamer s : listStreamers) {
-                addStreamerToMatrix(s);
-            }
-            drawCharMatrix(g,charWidth,charHeight);*/
-        } else {
-            // clear the char matrix
-            for (int row = 0; row < numRows; row++) {
-                for (int col = 0; col < numCols; col++) {
-                    charMatrix[row][col] = false;
-                }
-            }
-            for (Streamer s : listStreamers) {
-                drawStreamer(g2d, s, metrics.getAscent(), charWidth, charHeight);
-            }
+        //drawGridLines(g2d);
+        drawStreamers(g2d);
+    }
+    
+    private void drawGridLines(Graphics2D g2d) {
+        g2d.setColor(Color.WHITE);
+        // draw horizontal row lines
+        for (int row = 0; row < numRows; row++) {
+            g2d.drawLine(0,row*charHeight,getWidth(),row*charHeight);
+        }
+        // draw vertical column lines
+        for (int col = 0; col < numCols; col++) {
+            g2d.drawLine(col*charWidth,0,col*charWidth,getHeight());
         }
     }
     
-    // ------- alternate rendering ----------
+    private void drawStreamers(Graphics2D g2d) {
+        // clear the cellsDrawnIn matrix
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                cellsDrawnIn[row][col] = false;
+            }
+        }
+        for (Streamer streamer : listStreamers) {
+            drawStreamer(g2d, streamer);
+        }
+    }
     
-    private void drawStreamer(Graphics2D g2d, Streamer s, int ascent, 
-            int charWidth, int charHeight) {
+    private void drawStreamer(Graphics2D g2d, Streamer s) {
         
         for (int i = 0; i < s.text.length(); i++) {
-            Color col = s.fSpeed < 15.0f ? DARK_GREEN : LIGHT_GREEN; // decide what
+            Color col = s.speed < 15.0f ? DARK_GREEN : LIGHT_GREEN; // decide what
             // color to draw the streamer in based upon its speed
             if (i == 0) {
                 col = Color.WHITE; // head of the streamer is white
@@ -201,25 +194,16 @@ public class MatrixRainGUI extends JPanel {
                 // head are grey
             }
             g2d.setColor(col);
-            int nCharIndex = Math.abs( (i - ((int)s.fPosition)) ) % s.text.length();
+            int nCharIndex = Math.abs( (i - ((int)s.row)) ) % s.text.length();
             
-            int row = (int) s.fPosition - i;
-            
-            if (row >= 0 && row < numRows && charMatrix[row][s.column] == false) {
+            int rowDrawingIn = ((int)s.row) - i;
+            if (rowDrawingIn >= 0 && rowDrawingIn < numRows && cellsDrawnIn[rowDrawingIn][s.column] == false) {
+                // only draw a character at a given row/col position if a character
+                // has not already been drawn at that position
                 g2d.drawString("" + s.text.charAt(nCharIndex), 
-                    s.column * charWidth, row * charHeight);
-                charMatrix[row][s.column] = true;
+                    s.column * charWidth, rowDrawingIn * charHeight);
+                cellsDrawnIn[rowDrawingIn][s.column] = true; 
             }
-            
-            /*if (charMatrix[row][s.column] != false) {
-                return; // don't draw character in cell if there is already one present
-            }
-            
-            //g2d.drawString("" + s.text.charAt(nCharIndex), 
-                    //s.column * charWidth, (s.fPosition - i) * charHeight);
-            g2d.drawString("" + s.text.charAt(nCharIndex), 
-                    s.column * charWidth, row * charHeight);
-            charMatrix[row][s.column] = true;*/
         }
     }
     
@@ -228,34 +212,4 @@ public class MatrixRainGUI extends JPanel {
         //return (char) HelperMethods.mathRandom(33,128); // random ASCII character between 33 and 127
         return (char) HelperMethods.mathRandom(0x0400,0x0527); // character in the 'cryllic' character set
     }
-    
-    // ------------- char matrix methods -----------------
-    
-    /*private void clearCharMatrix() {
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                charMatrix[row][col] = ' ';
-            }
-        }
-    }
-    
-    // draw the char matrix to the screen
-    private void drawCharMatrix(Graphics g, int charWidth, int charHeight) {
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                g.drawString("" + charMatrix[row][col], col*charWidth, row*charHeight);
-            }
-        }
-    }
-    
-    private void addStreamerToMatrix(Streamer s) {
-        for (int i = 0; i < s.text.length(); i++) {
-            char c = s.text.charAt(i);
-            int row = (int) s.fPosition - i;
-            if (row < 0 || row >= numRows) {
-                continue;
-            }
-            charMatrix[row][s.column] = c;
-        }
-    }*/
 }
